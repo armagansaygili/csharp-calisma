e876634f29e72e7af3052fb26eb50214a1fa9c4b
armagansaygili <armaganozan@gmail.com>
2021-11-06 20:50:08 +0300

    Baslangic

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminiz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminiz:");
+            string sname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba" + name + " " + sname);
+
+        }
+    }
+}
